db77e2a347af9c81e5ed969d128a0b5e8bd10b54
himanshuGaur684 <60474413+himanshuGaur684@users.noreply.github.com>
2021-04-08 16:00:05 +0530

    Initial commit

---
+++ app/src/main/java/gaur/himanshu/august/moviedetails/ui/movie/MovieFragment.kt
@@ -0,0 +1,27 @@
+package gaur.himanshu.august.moviedetails.ui.movie
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import gaur.himanshu.august.moviedetails.R
+
+
+class MovieFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movie, container, false)
+    }
+
+
+}
