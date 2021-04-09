4473efc9bdfdaccd4f7c4756055c6570125f0568
himanshuGaur684 <60474413+himanshuGaur684@users.noreply.github.com>
2021-04-09 15:23:27 +0530

    Paging Data Adapter for our Project

---
+++ app/src/main/java/gaur/himanshu/august/moviedetails/ui/movie/MoviePagingAdapter.kt
@@ -0,0 +1,41 @@
+package gaur.himanshu.august.moviedetails.ui.movie
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import gaur.himanshu.august.moviedetails.BR
+import gaur.himanshu.august.moviedetails.data.Movie
+import gaur.himanshu.august.moviedetails.databinding.ViewHolderMovieBinding
+
+class MoviePagingAdapter : PagingDataAdapter<Movie, MoviePagingAdapter.MyViewHolder>(DIFF_UTIL) {
+
+
+    companion object {
+        val DIFF_UTIL = object : DiffUtil.ItemCallback<Movie>() {
+            override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+                return oldItem.imdbID == newItem.imdbID
+            }
+
+            override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+
+    inner class MyViewHolder(val viewDataBinding: ViewHolderMovieBinding) :
+        RecyclerView.ViewHolder(viewDataBinding.root)
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.viewDataBinding.setVariable(BR.movie, getItem(position))
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding =
+            ViewHolderMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
+    }
+
+}
